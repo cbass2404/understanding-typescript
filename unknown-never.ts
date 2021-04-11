0d10e47931f47806b0405d7777bf71aa44f1c091
Cory Bass <cory.bass.07@gmail.com>
2021-04-11 16:06:34 -0500

    finished unknown never demo

--- unknown-never.ts
+++ unknown-never.ts
@@ -1,11 +1,14 @@
-var userInput;
-var userName;
+let userInput: unknown;
+let userName: string;
+
 userInput = 5;
 userInput = "Max";
 if (typeof userInput === "string") {
     userName = userInput;
 }
-var generateError = function (message, code) {
+
+const generateError = (message: string, code: number): never => {
     throw { message: message, errorCode: code };
 };
+
 generateError("An error occurred", 500);
