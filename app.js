a16279499cf6ad9b2845e71e6f75a7450e2aa2a4
Cory Bass <cory.bass.07@gmail.com>
2021-04-11 15:55:23 -0500

    finished functions demo

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var combine = function (input1, input2, resultType) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultType === "as-number") {
-        result = +input1 + +input2;
-        return +result;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-        return result.toString();
-    }
+var add = function (n1, n2) {
+    return n1 + n2;
 };
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine(30, 26, "as-text");
-console.log(combinedStringAges);
-var combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+var printResult = function (num) {
+    console.log("Result: " + num);
+};
+var addAndHandler = function (n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+};
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+addAndHandler(10, 20, function (result) {
+    console.log(result);
+});
